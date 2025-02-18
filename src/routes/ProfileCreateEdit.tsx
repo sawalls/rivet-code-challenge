e38d00c1f3e40ad6f8f5418765ba3a2d8f569550
Schala Walls <sawalls@umich.edu>
2025-02-17 22:08:19 -0500

    fills profileEdit with profile requested's info by default

--- src/routes/ProfileCreateEdit.tsx
+++ src/routes/ProfileCreateEdit.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Form, useNavigate, useParams } from "react-router-dom";
-import { useCreateProfileMutation, useEditProfileMutation } from "../features/profile/profileApi";
+import { useCreateProfileMutation, useEditProfileMutation, useGetProfileByIdQuery } from "../features/profile/profileApi";
+import { type Profile } from '../features/profile/profileUtils';
 
 // TODO: error handling for
 // - network to create request fails
+// TODO: flesh this out a bit more. Error / loading states, etc.
 
 export const ProfileCreate = () => {
   return <ProfileCreateEdit mutation={useCreateProfileMutation()} verb="create" />;
 }
 
 export const ProfileEdit = () => {
-  return <ProfileCreateEdit mutation={useEditProfileMutation()} verb="edit" />;
+  const { id: profileId } = useParams();
+  // TODO: code duplication with Profile.tsx
+  const profileResult = useGetProfileByIdQuery(profileId);
+  const profile = profileResult.isSuccess ? profileResult.data : null;
+  // TODO: validate profileId exists, is a number, corresponds to a profile
+
+  return <ProfileCreateEdit mutation={useEditProfileMutation()} verb="edit" initialProfile={profile} />;
 }
 
-function ProfileCreateEdit({mutation, verb} : { mutation: ReturnType<typeof useCreateProfileMutation>, verb: 'create' | 'edit'}) {
+type ProfileVerb = 'create' | 'edit';
+
+interface ProfileCreateEditProps {
+  mutation: ReturnType<typeof useCreateProfileMutation> | ReturnType<typeof useEditProfileMutation>;
+  verb: ProfileVerb;
+  initialProfile?: Profile | undefined;
+}
+
+function ProfileCreateEdit({ mutation, verb, initialProfile }: Readonly<ProfileCreateEditProps>) {
   const { id: profileId } = useParams();
   const [mutateProfile, { isError, error }] = mutation;
   const navigate = useNavigate();
@@ -52,13 +68,17 @@ function ProfileCreateEdit({mutation, verb} : { mutation: ReturnType<typeof useC
     }
   } else {
     // isUninitialized || isLoading
-    return <ProfileForm handleSubmit={handleSubmit} verb={verb} />;
+    return <ProfileForm handleSubmit={handleSubmit} verb={verb} initialProfile={initialProfile} />;
   }
 }
 
-function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void , verb: 'create' | 'edit'}) {
-  const capsVerb = verb.charAt(0).toUpperCase() + verb.slice(1);
+interface ProfileFormProps {
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  verb: ProfileVerb;
+  initialProfile?: Profile | undefined;
+}
 
+function ProfileForm({ handleSubmit, verb, initialProfile }: Readonly<ProfileFormProps>) {
   return (
     <Form id={`profile-${verb}`} onSubmit={handleSubmit}>
       <p>
@@ -67,12 +87,14 @@ function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormE
           placeholder="First Name"
           type="text"
           name="first_name"
+          defaultValue={initialProfile?.first_name}
           required
         />
         <input
           placeholder="Last Name"
           type="text"
           name="last_name"
+          defaultValue={initialProfile?.last_name}
           required
         />
         {/*<input type="hidden" name="id">{id}</input>*/}
@@ -84,6 +106,7 @@ function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormE
             placeholder="(555) 555-0199"
             type="text"
             name="phone"
+            defaultValue={initialProfile?.phone}
             required
           />
         </label>
@@ -93,6 +116,7 @@ function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormE
             placeholder="replace_this@fictitious.example"
             type="text"
             name="email"
+            defaultValue={initialProfile?.email}
             required
           />
         </label>
@@ -104,21 +128,22 @@ function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormE
             placeholder="221B Replace Me"
             type="text"
             name="address"
+            defaultValue={initialProfile?.address}
             required
           />
         </label>
         <label>
           <span>City</span>
-          <input placeholder="Faketown" type="text" name="city" required />
+          <input placeholder="Faketown" type="text" name="city" defaultValue={initialProfile?.city} required />
         </label>
         <br />
         <label>
           <span>State</span>
-          <input placeholder="ZZ" type="text" name="state" required />
+          <input placeholder="ZZ" type="text" name="state" defaultValue={initialProfile?.state} required />
         </label>
         <label>
           <span>Zip</span>
-          <input placeholder="11111" type="text" name="zip" required />
+          <input placeholder="11111" type="text" name="zip" defaultValue={initialProfile?.zip} required />
         </label>
       </p>
       <p>
@@ -128,6 +153,7 @@ function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormE
             placeholder="fakeurl.fictitious.example/photo.jpg"
             type="text"
             name="photo"
+            defaultValue={initialProfile?.photo}
             required
           />
         </label>
@@ -138,12 +164,13 @@ function ProfileForm({ handleSubmit, verb }: { handleSubmit: (event: React.FormE
           <textarea
             placeholder="Your freeform notes go here"
             name="notes"
+            defaultValue={initialProfile?.notes}
             required
           />
         </label>
       </p>
       <p>
-        <button type="submit">{capsVerb}</button>
+        <button type="submit">Submit</button>
       </p>
     </Form>
   );
