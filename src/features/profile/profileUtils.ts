3549c68a02a4e045a8502750bea37b8ac6d6dac0
Schala Walls <sawalls@umich.edu>
2025-02-24 19:03:00 -0500

    removes some strange quotation marks

--- src/features/profile/profileUtils.ts
+++ src/features/profile/profileUtils.ts
@@ -2,16 +2,16 @@
 // I made this judgment call: the original fake data had no state nor photos, and notes were optionally their websites.
 // So, I made state, photo, and notes optional.
 type ProfileNoId = {
-  first_name: string; // 255 char max / required",
-  last_name: string; // 255 char max / required",
-  phone: string; // 255 char max / required",
-  email: string; // 255 char max / required",
-  address: string; // 255 char max / required",
-  city: string; // 255 char max / required",
-  state?: string; // 2 chars / optional",
-  zip: string; // 255 char max / required",
-  photo?: string; // 255 char max / URL to image file / optional",
-  notes?: string; // 4GB max / optional",
+  first_name: string; // 255 char max / required,
+  last_name: string; // 255 char max / required,
+  phone: string; // 255 char max / required,
+  email: string; // 255 char max / required,
+  address: string; // 255 char max / required,
+  city: string; // 255 char max / required,
+  state?: string; // 2 chars / optional,
+  zip: string; // 255 char max / required,
+  photo?: string; // 255 char max / URL to image file / optional,
+  notes?: string; // 4GB max / optional,
 };
 
 interface Profile extends ProfileNoId {
