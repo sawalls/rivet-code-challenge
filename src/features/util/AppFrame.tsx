de67979b715fb797c834c61629dd4a322b114e25
Schala Walls <sawalls@umich.edu>
2025-02-24 19:03:04 -0500

    changes the header to an AppBar from MUI

--- src/features/util/AppFrame.tsx
+++ src/features/util/AppFrame.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
 import { Link } from "react-router-dom";
+import CameraEnhanceIcon from "@mui/icons-material/CameraEnhance";
 
 interface AppFrameProps {
   children: React.ReactNode;
@@ -10,11 +11,33 @@ function AppFrame({ children }: AppFrameProps) {
   return (
     <div className="App" style={{ textAlign: "center" }}>
       <header className="App-header">
-        <Box>
-          <Link to={"/"}>
-            <h1>Schala Challenge Solution</h1>
-          </Link>
-        </Box>
+        <Box
+          sx={{
+            flexGrow: 1,
+            width: "50em",
+            boxSizing: "border-box",
+            padding: ".5em",
+            margin: "0 auto",
+            maxWidth: "100%",
+            alignItems: "center",
+          }}
+        >
+          <AppBar position="static">
+            <Toolbar sx={{ justifyContent: "center" }}>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+              >
+                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                  <CameraEnhanceIcon />
+                  &nbsp;schalafile
+                </Link>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>{" "}
       </header>
       {children}
     </div>
