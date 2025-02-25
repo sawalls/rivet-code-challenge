cacd580dea365437e61d5e21483a2bdcd77ad8aa
Schala Walls <sawalls@umich.edu>
2025-02-24 19:03:01 -0500

    uses Omit to make ProfileNoId from Profile instead of adding to ProfileNoId to get Profile

--- src/features/profile/profileUtils.ts
+++ src/features/profile/profileUtils.ts
@@ -1,7 +1,8 @@
 // Note: to flex a bit that I know the difference between required and non-required fields,
 // I made this judgment call: the original fake data had no state nor photos, and notes were optionally their websites.
 // So, I made state, photo, and notes optional.
-type ProfileNoId = {
+type Profile = {
+  id: number; // positive integer / required,
   first_name: string; // 255 char max / required,
   last_name: string; // 255 char max / required,
   phone: string; // 255 char max / required,
@@ -14,8 +15,6 @@ type ProfileNoId = {
   notes?: string; // 4GB max / optional,
 };
 
-interface Profile extends ProfileNoId {
-  id: number;
-}
+type ProfileNoId = Omit<Profile, "id">;
 
 export type { Profile, ProfileNoId };
